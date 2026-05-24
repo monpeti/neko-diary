f7f8a0a43a5c233d82a785a29fcfa4e60ee91ac6
モンペティ <caloriefate6114@yahoo.co.jp>
2026-05-24 16:28:24 +0900

    root変更

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DiaryController; // 猫日記ページのルートを追加
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/diaries');
 });
 
 // 猫日記ページのルートを追加
